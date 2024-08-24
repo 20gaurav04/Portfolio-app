a52026fb9f91aeca82320578fd9c7bb1a5b8d3f9
20gaurav04 <btechug2026@gmail.com>
2024-08-24 20:25:00 +0530

    changed intro button from hireME to Resume

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import './Intro.css';
-import hireMe from '../asset/hireme.png'; /* Adjusted to relative path */
-import reactLogo from '../asset/react1.png'; /* Adjusted to relative path */
+import resumeIcon from '../asset/hireme.png';  // Assuming you're using the same icon, you can rename the variable to better reflect its new purpose
+import reactLogo from '../asset/react1.png';
 
 function Intro() {
   const [text] = useTypewriter({
@@ -10,12 +10,17 @@ function Intro() {
     loop: true,
   });
 
+  const handleResumeClick = () => {
+
+    window.open('https://docs.google.com/document/d/12vtqqdOZq51N_y9Nsp7TLctcUliZDHG9/edit?usp=drive_link&ouid=115979587760637855097&rtpof=true&sd=true', '_blank'); 
+  };
+
   return (
     <section className="section" id="nav">
       <span className="intro">Creating <span className="sleek">SLEEK</span>, responsive designs</span>
       <span className="subIntro">{text} <Cursor className="cursor" cursorStyle="|"/></span>
-      <button className="hireMe" onClick={()=> {document.getElementById('').scrollIntoView({behavior:'smooth'})}}>
-        <img src={hireMe} alt='Hire Me' className='hireMeImg'/>Hire Me
+      <button className="resumeButton" onClick={handleResumeClick}>
+        <img src={resumeIcon} alt='Resume' className='resumeImg'/>Resume
       </button>
       <img src={reactLogo} alt="logo" className="reactLogo"></img>
     </section>
